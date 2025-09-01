25b448eb7dce2fe696289c2b45c66cf1dd1d4b97
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-08-31 23:23:46 -0300

    criando comando de login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,5 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', () => {
+    cy.get('[data-test="input-loginEmail"]').type('leonardo.laurindo@email.com')
+    cy.get('[data-test="input-loginPassword"]').type('Leo123')
+    cy.get('[data-test="submit-button"]').click()
+})
