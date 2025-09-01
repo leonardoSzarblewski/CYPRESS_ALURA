8f345f2b81e4e998003d3d753232dd72df10e296
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-08-31 23:27:02 -0300

    cenário email e senha inválida no login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -16,4 +16,13 @@ describe('Página de login', () => {
         cy.contains('Veja os amigos disponíveis para adoção!').should('be.visible')
     });
 
+    it('Deve falhar ao fazer login com e-mail e senha inválido', () => {
+        cy.get('[data-test="input-loginEmail"]').type('Inválido')
+        cy.get('[data-test="input-loginPassword"]').type('Inválido')
+        cy.get('[data-test="submit-button"]').click()
+
+        cy.contains('Por favor, verifique o email digitado').should('be.visible')
+        cy.contains('A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres')
+        .should('be.visible')
+    });
 });
